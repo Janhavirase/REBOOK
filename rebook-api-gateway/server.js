0224da877ff906d9831c82191c0d50287fb8cb39
Janhavirase <149667687+Janhavirase@users.noreply.github.com>
2026-06-29 13:25:10 +0530

    fix cors

--- rebook-api-gateway/server.js
+++ rebook-api-gateway/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://rebook-xi.vercel.app" 
+      "rebook-gamma.vercel.app"
     ],
     credentials: true
 }));
